0080fd6e922213307effec3915a19b8adee1b7f6
Karim Alaa <karim.hamed89@gmail.com>
2014-05-04 11:06:33 +0200

    changed post flag user id to integer

--- db/migrate/20140220101222_create_api_v1_post_flags.rb
+++ db/migrate/20140220101222_create_api_v1_post_flags.rb
@@ -1,7 +1,7 @@
 class CreateApiV1PostFlags < ActiveRecord::Migration
   def change
     create_table :post_flags do |t|
-      t.string :user_id
+      t.integer :user_id
       t.integer :post_id
 
       t.timestamps
